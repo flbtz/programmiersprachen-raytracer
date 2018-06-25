7cf41a9e0fff623603710db450050f183cd0cc02
Florian B <florian_batze@gmx.de>
2018-06-25 13:25:10 +0200

    added 5.1

---
+++ cmake-build-debug/framework/CMakeFiles/framework.dir/DependInfo.cmake
@@ -0,0 +1,56 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/Flo/Documents/Studium/Programmiersprachen/programmiersprachen_aufgabe5/programmiersprachen-raytracer/framework/glew.c" "/Users/Flo/Documents/Studium/Programmiersprachen/programmiersprachen_aufgabe5/programmiersprachen-raytracer/cmake-build-debug/framework/CMakeFiles/framework.dir/glew.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "GLEW_STATIC"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../external"
+  "../external/glfw-3.2.1/include"
+  "../external/nanovg/src"
+  "../external/catch-1.5"
+  "../external/glm-0.9.5.3"
+  "../framework"
+  "../framework/.."
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/Flo/Documents/Studium/Programmiersprachen/programmiersprachen_aufgabe5/programmiersprachen-raytracer/framework/pixel.cpp" "/Users/Flo/Documents/Studium/Programmiersprachen/programmiersprachen_aufgabe5/programmiersprachen-raytracer/cmake-build-debug/framework/CMakeFiles/framework.dir/pixel.cpp.o"
+  "/Users/Flo/Documents/Studium/Programmiersprachen/programmiersprachen_aufgabe5/programmiersprachen-raytracer/framework/ppmwriter.cpp" "/Users/Flo/Documents/Studium/Programmiersprachen/programmiersprachen_aufgabe5/programmiersprachen-raytracer/cmake-build-debug/framework/CMakeFiles/framework.dir/ppmwriter.cpp.o"
+  "/Users/Flo/Documents/Studium/Programmiersprachen/programmiersprachen_aufgabe5/programmiersprachen-raytracer/framework/renderer.cpp" "/Users/Flo/Documents/Studium/Programmiersprachen/programmiersprachen_aufgabe5/programmiersprachen-raytracer/cmake-build-debug/framework/CMakeFiles/framework.dir/renderer.cpp.o"
+  "/Users/Flo/Documents/Studium/Programmiersprachen/programmiersprachen_aufgabe5/programmiersprachen-raytracer/framework/window.cpp" "/Users/Flo/Documents/Studium/Programmiersprachen/programmiersprachen_aufgabe5/programmiersprachen-raytracer/cmake-build-debug/framework/CMakeFiles/framework.dir/window.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "GLEW_STATIC"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../external"
+  "../external/glfw-3.2.1/include"
+  "../external/nanovg/src"
+  "../external/catch-1.5"
+  "../external/glm-0.9.5.3"
+  "../framework"
+  "../framework/.."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/framework/CMakeFiles/framework.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/framework.dir/glew.c.o"
+  "CMakeFiles/framework.dir/pixel.cpp.o"
+  "CMakeFiles/framework.dir/ppmwriter.cpp.o"
+  "CMakeFiles/framework.dir/renderer.cpp.o"
+  "CMakeFiles/framework.dir/window.cpp.o"
+  "libframework.pdb"
+  "libframework.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/framework.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
